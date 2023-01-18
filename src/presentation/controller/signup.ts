4d09e3bdc0424bf99dcc1af901c98d19a895a81a
Deyvid Nascimento <deyvin@gmail.com>
2023-01-18 17:06:41 -0300

    refactor: body params and camelize password_confirmation

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -12,18 +12,20 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ['name', 'email', 'password', 'password_confirmation']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.password_confirmation) {
-        return badRequest(new InvalidParamError('password_confirmation'))
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
